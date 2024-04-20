7eae327bfdc8493c371505c741b3d47413ae76d8
hamditug <hamditugmobil@gmail.com>
2024-04-20 22:16:21 +0300

    Initial Commit

---
+++ TodoIOS/TodoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoIOSApp.swift
+//  TodoIOS
+//
+//  Created by Hamdi Tuğ on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
